cd64e09db635e878e7cf5931fc4935dc46ec4b51
rheyan keneth <rpahuyo@gmail.com>
2020-02-20 22:13:31 +0800

    Initial Commit

---
+++ Application/StringLibrary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Application
+{
+    public static class StringLibrary
+    {
+        public static string TextToPreview(this string value)
+        {
+            return !string.IsNullOrEmpty(value) ? value.Split()[0] + " ... " : " ... ";
+        }
+    }
+}
